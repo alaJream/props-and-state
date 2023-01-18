5a0a0486713c571e19c8563fe1938015adc15d26
alaJream <jeremy.gonzales@students.dominican.edu>
2023-01-18 13:59:28 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css';
+import Counter from './Counter'
+
+function App() {
+  const [count, setCount] = useState([1,4,3])
+  
+  return (
+    
+    <div className="App">
+      <h1>{count.reduce((acc, val) => acc + val)}</h1>
+      {count.map((value, index) => {
+        return (
+          <Counter 
+            label={`counter ${index}`}
+            value={value} 
+
+            increment={() => {
+              const newCount = [...count]
+              newCount[index] += 1
+              setCount(newCount)
+            }}
+            decrement={() => {
+              const newCount = [...count]
+              newCount[index] -= 1
+              setCount(newCount)
+            }}
+
+          />)
+      })}
+    </div>
+  );
+  
+}
+
+export default App;
